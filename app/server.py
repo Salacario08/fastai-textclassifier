c29bf3bd58c1e05e4a02de0c28cef0031ab459bd
Salacario08 <36950912+Salacario08@users.noreply.github.com>
2020-04-13 08:56:36 +0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -12,7 +12,7 @@ from starlette.staticfiles import StaticFiles
 export_file_url = 'https://drive.google.com/uc?export=download&id=1GjJSbv8LsX6xCqArYr4rdnpbOif4GBKu'
 export_file_name = 'hipxmet.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['Hip-Hop', 'Metal']
 path = Path(__file__).parent
 
 app = Starlette()
