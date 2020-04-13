5dcbdde1a36847f0b50e9d37ed72582d8c30ce82
Salacario08 <36950912+Salacario08@users.noreply.github.com>
2020-04-13 09:15:36 +0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/uc?export=download&id=1GjJSbv8LsX6xCqArYr4rdnpbOif4GBKu'
-export_file_name = 'hipxmet.pkl_'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1deKBRAexXNOCxI8uIsG-UVLjToEaOO8r'
+export_file_name = 'export.pkl'
 
-classes = ["Hip-Hop" , "Metal"]
+classes = ['black', 'grizzly', 'teddys']
 path = Path(__file__).parent
 
 app = Starlette()
