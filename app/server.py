2c33343ed5adabf72e914689c9139f9b6f1fb578
Salacario08 <36950912+Salacario08@users.noreply.github.com>
2020-04-13 09:40:10 +0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -10,7 +10,7 @@ from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
 export_file_url = 'https://drive.google.com/uc?export=download&id=1YjW3-u1R4QDh2HyOq0EFVgjxmtAgHTGM'
-export_file_name = 'export.pkl'
+export_file_name = 'hipxmet.pkl_'
 
 classes = ['black', 'grizzly', 'teddys']
 path = Path(__file__).parent
