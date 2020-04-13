537ce9b01b919a478b95ca5702a38db3d3fb040a
Salacario08 <36950912+Salacario08@users.noreply.github.com>
2020-04-13 09:52:48 +0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,6 +9,13 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
+from functools import partial
+import pickle
+pickle.load = partial(pickle.load, encoding="latin1")
+pickle.Unpickler = partial(pickle.Unpickler, encoding="latin1")
+model = torch.load(model_file, map_location=lambda storage, loc: storage, pickle_module=pickle)
+
+
 export_file_url = 'https://drive.google.com/uc?export=download&id=1GM-c5CZ8zKYQ0fA5pefW3ydxdUZugGlb'
 export_file_name = 'hipxmet.pkl'
 
